0949d879a1d781264365a9434caf2007a25ac74e
younan <okokoknow@naver.com>
2020-07-15 23:57:35 +0900

    [Scala] Study Higher-order Functions

--- src/high/order/functions/MapObject.scala
+++ src/high/order/functions/MapObject.scala
@@ -11,8 +11,20 @@ object MapObject {
     newSalaries = salaries.map(x => x * 3)
     println(newSalaries)
 
-    salaries = Seq(1,2,3)
+    salaries = Seq(1, 2, 3)
     newSalaries = salaries.map(_ * 2)
     println(newSalaries)
   }
+
+  private def promotion(salaries: List[Double], promotionFunction: Double => Double): List[Double] =
+    salaries.map(promotionFunction)
+
+  def smallPromotion(salaries: List[Double]): List[Double] =
+    promotion(salaries, salary => salary * 1.1)
+
+  def greatPromotion(salaries: List[Double]): List[Double] =
+    promotion(salaries, salary => salary * math.log(salary))
+
+  def hugePromotion(salaries: List[Double]): List[Double] =
+    promotion(salaries, salary => salary * salary)
 }
